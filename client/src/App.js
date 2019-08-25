6f1f2b3c79d0d9c835e58f4ecc12a0d8832879b4
Junaid Farooq <junaid@evercam.io>
2019-08-25 09:16:15 +0500

    Added AppNavbar.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import AppNavbar from "./components/AppNavbar";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <AppNavbar />
     </div>
   );
 }
